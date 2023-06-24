d0de8f6091dc17dbc2d693eddd231971917e0bcf
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2023-06-24 19:13:00 +0530

    Update Logout.js

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -11,7 +11,7 @@ const Logout = () => {
             method: "GET",
             headers: {
                 Accept: "application/json",
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
                 "Access-Control-Allow-Origin": "access-control-allow-origin"
             },
             credentials: "include"
