0104e3435dc9649e9888766ea61da7246560370a
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2023-06-27 09:55:59 +0530

    Update auth.js

--- server/router/auth.js
+++ server/router/auth.js
@@ -81,7 +81,7 @@ router.post('/login', async (req, res) => {
             res.cookie("jwtoken", token, {
                 expires: new Date(Date.now() + 25992000000000),
                 httpOnly: true,
-                secure: process.env.NODE_ENV === "production"
+                // secure: process.env.NODE_ENV === "production"
             });
             // console.log(userLogin);
             if (!isMatch) {
